182e0f3e75139335b50592f52815e804319c29ab
Mahmud <mahmudfoysalsumon824@gmail.com>
2019-04-06 21:47:06 +0600

    Adding navigation drawer

--- app/src/main/java/com/example/xyz/MainActivity.java
+++ app/src/main/java/com/example/xyz/MainActivity.java
@@ -1,13 +1,89 @@
 package com.example.xyz;
 
+import android.content.res.Configuration;
+import android.os.Build;
+import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Objects;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout mDrawer;
+    private ActionBarDrawerToggle mToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+        mDrawer = findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this,mDrawer,R.string.drawer_open,R.string.drawer_closed);
+        mDrawer.addDrawerListener(mToggle);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+            Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
+        }
+    }
+
+    @Override
+    public void onPostCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        mToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawer.isDrawerOpen(GravityCompat.START)){
+            mDrawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        if (menuItem.getItemId() == R.id.nav_item_one){
+            Toast.makeText(this, "item_one", Toast.LENGTH_SHORT).show();
+        }
+        if (menuItem.getItemId() == R.id.nav_item_two){
+            Toast.makeText(this, "item_two", Toast.LENGTH_SHORT).show();
+        }
+        if (menuItem.getItemId() == R.id.nav_item_three){
+            Toast.makeText(this, "item_three", Toast.LENGTH_SHORT).show();
+        }
+        if (menuItem.getItemId() == R.id.nav_item_four){
+            Toast.makeText(this, "item_four", Toast.LENGTH_SHORT).show();
+        }
+        if (menuItem.getItemId() == R.id.nav_item_five){
+            Toast.makeText(this, "item_five", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
